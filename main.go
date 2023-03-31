35c74b9b22d1c8f6d23e0533a62083f6bdf911ec
Masih H. Derkani <m@derkani.org>
2023-03-31 15:37:16 +0100

    Add CLI flags to set http IPNI endpoint and parallelism
    Add flags to set most needed options.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,7 +10,11 @@ import (
 
 func main() {
 	// TODO: add flags for each of the options and pass to newHeyFil
-	hf, err := newHeyFil()
+	httpIndexerEndpoint := flag.String("httpIndexerEndpoint", "https://cid.contact", "The HTTP IPNI endpoint to which announcements are made.")
+	maxConcurrentChecks := flag.Int("maxConcurrentChecks", 10, "The maximum number of concurrent checks.")
+	flag.Parse()
+
+	hf, err := newHeyFil(WithHttpIndexerEndpoint(*httpIndexerEndpoint), WithMaxConcurrentChecks(*maxConcurrentChecks))
 	if err != nil {
 		panic(err)
 	}
